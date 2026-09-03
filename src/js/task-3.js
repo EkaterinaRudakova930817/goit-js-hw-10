db5de51995f7e4010e4f444451ef5df50bafcf38
Kateryna Rudakova <ekaterinarudakova170893@gmail.com>
2026-09-03 19:21:14 +0200

    Complete homework 10

--- src/js/task-3.js
+++
@@ -1,8 +0,0 @@
-const nameInput = document.querySelector('#name-input');
-const nameOutput = document.querySelector('#name-output');
-
-nameInput.addEventListener('input', event => {
-  const name = event.currentTarget.value.trim();
-
-  nameOutput.textContent = name === '' ? 'Anonymous' : name;
-});
